88bb30b5e2b7c7e76c8b3704c9ff1d109e10fced
Sylvia <sylviad1221@gmail.com>
2017-04-14 01:29:15 -0400

    add headers

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set headers to prevent CORS errors
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Request-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  // and remove cacheing so we get the most recent comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/movies', movies);
