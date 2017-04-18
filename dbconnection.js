93ee8540f5ab651ef62fc617781b0ef4570f54a6
Sylvia <sylviad1221@gmail.com>
2017-04-18 19:44:37 -0400

    add cleardb database/heroku add on

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'movie_buff'
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'be1425ce243e0a',
+  password: '25debf5a',
+  database: 'heroku_d399dc57a8d6b87'
 });
 
 module.exports = connection;
